a5448f98a2091345116d4aba11090092017b4c14
Andrews Osei Kontoh <kontoh.2@osu.edu>
2023-07-31 23:24:56 -0400

    Implemented controller and service layer

---
+++ src/main/java/com/api/demoapi/DemoapiApplication.java
@@ -0,0 +1,18 @@
+package com.api.demoapi;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+@SpringBootApplication
+@RestController
+public class DemoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoapiApplication.class, args);
+	}
+
+	
+
+}
---
+++ src/main/java/com/api/demoapi/student/StudentService.java
@@ -0,0 +1,16 @@
+package com.api.demoapi.student;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+@Service
+public class StudentService {
+    
+    public List<Student> getStudents(){
+        return List.of(
+            new Student(1L, "Mariam", "Mariam@gmail.com", LocalDate.of(2000, Month.JANUARY, 5), 20));
+        
+    }
+}
